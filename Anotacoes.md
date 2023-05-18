1993f92c08ead8a7cf76edde9645051e289c19c6
hesauhugo <hesau.hugo@hotmail.com>
2023-05-18 10:29:52 -0300

    feat: desserealizando objeto

--- Anotacoes.md
+++ Anotacoes.md
@@ -43,3 +43,20 @@
 * espaços  aumentam o tamanho do arquivo
 ## Datatime
 * Csharp representa o padrão ISO 8601
+```csharp
+
+    var dataAtual  = DateTime.Now;
+    var listaVendas = new List<Venda>();
+    var venda1 = new Venda(1,"Produto 1", 10.00M,dataAtual);
+    var venda2 = new Venda(2,"Produto 2", 20.00M,dataAtual);
+
+    listaVendas.Add(venda1);
+    listaVendas.Add(venda2);
+
+    string serializado  = JsonConvert.SerializeObject(listaVendas,Formatting.Indented);
+    File.WriteAllText("Arquivos/listaVendas.json",serializado);
+    Console.WriteLine(serializado);
+
+```
+## Desserealizar um objeto
+* Primeiro você deve estudar o conteúdo do arquivo
--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
 ﻿using Dotnet_Nuget_Serializar_Atributos.Models;
 using Newtonsoft.Json;
 
-var dataAtual  = DateTime.Now;
-var listaVendas = new List<Venda>();
-var venda1 = new Venda(1,"Produto 1", 10.00M,dataAtual);
-var venda2 = new Venda(2,"Produto 2", 20.00M,dataAtual);
-
-listaVendas.Add(venda1);
-listaVendas.Add(venda2);
-
-string serializado  = JsonConvert.SerializeObject(listaVendas,Formatting.Indented);
-File.WriteAllText("Arquivos/listaVendas.json",serializado);
-Console.WriteLine(serializado);
+var conteudoArquivo = File.ReadAllText("Arquivos/listaVendas.json");
+List<Venda> listaVendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+foreach (var venda in listaVendas)
+{
+    Console.WriteLine($"Id: {venda.Id} Produto: {venda.Produto}");
+}
